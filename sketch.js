f6e66b8f05d35029de027048781cd6924213ba41
Sam <samxcrowley@gmail.com>
2021-02-03 14:56:56 +1100

    Renamed and restructured files. Terms and Operations now have their own folder. Operations no longer have 'operation' in the file name.

--- sketch.js
+++ sketch.js
@@ -37,10 +37,6 @@ function setup() {
     
     parser = new Parser();
     
-    var rpn = parser.infixToRPN("5^2+1");
-    console.log(rpn);
-    console.log(parser.calculateRPN(rpn));
-    
 }
 
 function calculate() {
@@ -61,28 +57,7 @@ function draw() {
     text(mainExpressionRPN, 10, 80);
     text(mainExpressionResult, 10, 110);
     
-    let div1 = new DivisionOperation(new ConstantTerm("5"), new ConstantTerm("2"));
-    let div2 = new DivisionOperation(new VariableTerm("x"), new AdditionOperation(new VariableTerm("z"), new ConstantTerm("2")));
-//    
-//    var mainExpression;
-//    
-//    mainExpression = new MultiplicationOperation(new ConstantTerm("5"), new ConstantTerm("2"));
-//    mainExpression = new AdditionOperation(new ConstantTerm("5"), new ConstantTerm("2"));
-//    mainExpression = new MultiplicationOperation(div1, div2);
-//    mainExpression = new AdditionOperation(div1, div2);
-//    
-//    mainExpression = new AdditionOperation(new MultiplicationOperation(new VariableTerm("x"), new ConstantTerm("2")),
-//                                          new AdditionOperation(div1, div2));
-    
-//    mainExpression = new MultiplicationOperation(new VariableTerm("x"), new ConstantTerm("5"));
-    
-//    mainExpression = new IndexOperation(new VariableTerm("x"), new ConstantTerm("2"));
-    mainExpression = new IndexOperation(new AdditionOperation(new VariableTerm("x"), new ConstantTerm("10")), new ConstantTerm("2"));
-    
-    let w = mainExpression.getDrawnWidth();
-    let h = mainExpression.getDrawnHeight();
-    
-    mainExpression.draw((WIDTH / 2) - (w / 2), (HEIGHT / 2) - (h / 2));
+    //mainExpressionObj.draw((WIDTH / 2) - (w / 2), (HEIGHT / 2) - (h / 2));
     
 }
 
