929428bd8b53e84ac353f16d2842b7a96254b923
Sam Crowley <samxcrowley@gmail.com()>
2021-02-03 13:06:31 +1100

    Began Parser class which will handle all parsing and calculating of expressions

---
+++ parse/parser.js
@@ -0,0 +1,20 @@
+// Infix notation e.g.: "A ^ 2 + 2 * A * B + B ^ 2"
+// RPN notation e.g.: "A 2 ^ 2 A * B * + B 2 ^ +"
+// infixToRPN algorithm adapted from https://www.andreinc.net/2010/10/05/converting-infix-to-rpn-shunting-yard-algorithm
+// calculateRPN algorithm adapted from https://codereview.stackexchange.com/questions/120451/reverse-polish-notation-calculator-in-java
+
+class Parser {
+    
+    // converts a string in Infix notation to RPN notation
+    infixToRPN(infixString) {
+        
+        
+        
+    }
+    
+    // returns the result after calculating an RPN string
+    calculateRPN() {
+        
+    }
+    
+}
