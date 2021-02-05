b1413c92926d8e0e7e5a93af7e64028c30afd80b
Sam <samxcrowley@gmail.com>
2021-02-05 14:11:44 +1100

    Fixed issue where subtraction symbol was no overriding addition symbol of parent

--- expression/operation/subtraction.js
+++ expression/operation/subtraction.js
@@ -2,6 +2,7 @@ class SubtractionOperation extends AdditionOperation {
     
     constructor(valueOne, valueTwo) {
         super(valueOne, valueTwo, "-", false, 0);
+        this.symbol = "-";
     }
     
 }
